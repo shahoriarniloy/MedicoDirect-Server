724db47f04711266e917db2911ad020c40d4cef3
shahoriarniloy <niloyshahoriar@gmail.com>
2024-06-03 15:16:05 +0600

    Create Medicine Information

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -7,19 +6,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    
-],
+    'http://localhost:5174'
+  ],
   credentials: true
 };
 
 app.use(cors(corsOptions));
-
-
-
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxgrzuk.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,40 +26,49 @@ const client = new MongoClient(uri, {
   }
 });
 
-
 async function run() {
   try {
     const database = client.db("MedicoDirect");
     const categoryCollection = database.collection("categories");
-    
+    const medicinesCollection = database.collection("medicines");
 
-      
-     
-
-
-     
-
-    app.get('/categories',  async (req, res) => {
-        try {
-        
+    app.get('/categories', async (req, res) => {
+      try {
         const cursor = categoryCollection.find();
         const result = await cursor.toArray();
         res.json(result);
-        } catch (error) {
-        console.error("Error retrieving foods:", error);
+      } catch (error) {
+        console.error("Error retrieving categories:", error);
         res.status(500).json({ error: "Internal server error" });
-        }
-  });
+      }
+    });
 
-  
+    app.post('/medicines', async (req, res) => {
+      try {
+        const newMedicine = req.body;
+        const result = await medicinesCollection.insertOne(newMedicine);
+        res.status(201).json(result);
+      } catch (error) {
+        console.error('Error adding medicine:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
-
-
-
+    app.get('/medicines', async (req, res) => {
+      try {
+        const cursor = medicinesCollection.find();
+        const result = await cursor.toArray();
+        res.json(result);
+      } catch (error) {
+        console.error('Error retrieving medicines:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
   } finally {
-    
+    // Ensure the client will close when you finish/error
   }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
