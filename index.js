d5e26015e55c678498ad2f2692429893a20abc6b
shahoriarniloy <niloyshahoriar@gmail.com>
2024-06-03 21:42:42 +0600

    Cart Item Get

--- index.js
+++ index.js
@@ -107,6 +107,11 @@ async function run() {
       }
     });
 
+    app.get('/carts', async(req,res)=>{
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/carts', async(req,res)=>{
         const cartItem = req.body;
         const result = await cartCollection.insertOne(cartItem);
